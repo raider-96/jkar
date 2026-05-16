8729c0ca3c017596ed49383fabb0cee20094fd3b
9rdr <s2411130050@uowa.edu.iq>
2026-05-16 06:44:43 +0300

    f

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -12,20 +12,20 @@ export const CATEGORIES_CONFIG = [
   { name: 'أفلام أجنبي', icon: '/img/categories/afag.jpg' },  
   { name: 'مشاهير عرب', icon: '/img/categories/mshar.jpg' },
   { name: 'مشاهير أجانب', icon: '/img/categories/mshag.jpg' },
-  { name: 'ماركات', icon: '/img/categories/mark.jpg' },
+  { name: 'ماركات', icon: '/img/categories/mar.jpg' },
   { name: 'تحديات دين', icon: '/img/categories/den.jpg' },
   { name: 'تحديات حسينية', icon: '/img/categories/hs.jpg' },
   { name: 'سيارات', icon: '/img/categories/car.jpg' },
   { name: 'ألعاب الفيديو', icon: '/img/categories/gam.jpg' },
   { name: 'العراق', icon: '/img/categories/iq.jpg' },
   { name: 'كرة قدم  ', icon: '/img/categories/kra.jpg' },
-  { name: 'دوري اسباني', icon: '/img/categories/keaas.jpg' },
+  { name: 'دوري اسباني', icon: '/img/categories/kraas.jpg' },
   { name: 'دوري انكليزي', icon: '/img/categories/kraan.jpg' },
   { name: 'تاريخ', icon: '/img/categories/tar.jpg' },
   { name: 'جغرافية', icon: '/img/categories/ghr.jpg' },
   { name: 'عواصم ودول', icon: '/img/categories/dol.jpg' },
   { name: 'رياضيات وألغاز منطق', icon: '/img/categories/math.jpg' },
-  { name: 'لغة عربية', icon: '/img/categories/arb.jpg' },
+  { name: 'لغة عربية', icon: '/img/categories/ar.jpg' },
   { name: 'علوم', icon: '/img/categories/3lm.jpg' }
 ];
 
